6769fbae8a241728904e792682321b326477ae59
Ollie <wille667@morris.umn.edu>
2022-09-02 13:35:45 -0500

    Add full functionality of the encryption and decryption

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -2,39 +2,56 @@ import java.util.Scanner;
 
 public class Encrypt {
 
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter text:");
-        String text = scanner.nextLine();
-        System.out.print("Enter Key:");
-        String key = scanner.nextLine();
-        System.out.println(VigenereEncode(text.toLowerCase(), key));
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Encode or decode?");
+		String mode = scanner.nextLine();
+		System.out.print("Enter text:");
+		String text = scanner.nextLine();
+		System.out.print("Enter Key:");
+		String key = scanner.nextLine();
+		scanner.close();
+		if (mode.contains("enc")) {
+			System.out.println(VigenereEncode(text, key));
+		}else if(mode.contains("dec")) {
+			System.out.println(VigenereDecode(text,key));
+		}
+	}
 
-    }
+	public static String VigenereEncode(String text, String key) {
+		
+		int[] textAsInts = StringToIntArray(removePunct(text.toLowerCase())); 	// Convert messy string to clean int[] (a=0)
+		int[] keyAsInt = StringToIntArray(key.toLowerCase());					// Do the same for the key
+		int keyLength = key.length();
+		char[] encryptChars = new char[textAsInts.length];						// This is where we will store the encrypted
+		for (int i = 0; i < textAsInts.length; i++) {
+			encryptChars[i] = (char) ('a' + ((textAsInts[i] + keyAsInt[i % keyLength]) % 26));
+		}
 
+		return String.valueOf(encryptChars);
+	}
 
-public static char[] VigenereEncode(String text, String key) {
-    char[] cleanText = removePunct(text).toCharArray();
-    int[] textAsInts = StringToIntArray(cleanText);
-    int[] keyAsInt = StringToIntArray(key.toCharArray());
-    int keyLength = key.length();
-    int[] encryptChars = new int[cleanText.length];
-    for (int i = 0; i<textAsInts.length; i++){
-        encryptChars[i] = 'a'+(cleanText[i]+keyAsInt[i%keyLength])%26;
-    }
-    
-    return char[] (encryptChars);
-}
+	public static String removePunct(String text) {
+		return text.replaceAll("[^a-z]", "");
+	}
 
-public static String removePunct(String text) {
-    return text.replaceAll("[^a-z]", "");
-}
-
-public static int[] StringToIntArray (char[] cleanText){
-    int[] textAsInts = new int[cleanText.length];
-    for(int i = 0;i<cleanText.length;i++){
-        textAsInts[i] = cleanText[i] - 'a';
-    }
-    return textAsInts;
-}
+	public static int[] StringToIntArray(String cleanTextString) {
+		char[] cleanText = cleanTextString.toCharArray();
+		int[] textAsInts = new int[cleanText.length];
+		for (int i = 0; i < cleanText.length; i++) {
+			textAsInts[i] = cleanText[i] - 'a';
+		}
+		return textAsInts;
+	}
+	public static String VigenereDecode(String Ciphertext, String key) {
+		
+		int[] textAsInts = StringToIntArray(Ciphertext);
+		int[] keyAsInt = StringToIntArray(key.toLowerCase());	
+		int keyLength = key.length();
+		char[] decryptChars = new char[textAsInts.length];		
+		for (int i = 0; i < textAsInts.length; i++) {
+			decryptChars[i]= (char) ('a' + (textAsInts[i] + (26-keyAsInt[i % keyLength])) % 26);
+		}
+		return String.valueOf(decryptChars);
+	}
 }
